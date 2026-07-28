e5de73741b6bee5677dcae3bf15516b135bc9457
tsubokura2024 <139312872+tsubokura14@users.noreply.github.com>
2026-07-28 22:21:40 +0900

    feat: GCP移行に伴う変更

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // ビルド成果物をDockerコンテナで動かすことに最適化された必要最低限だけ出力
   turbopack: {
     root: path.join(__dirname),
   },
